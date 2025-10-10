954f07529791b489d9df62e9ccf777d9e2a6539f
Leoemanuel21 <leoemanuel14.rocha@gmail.com>
2025-10-10 09:35:10 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 function share(){
-    
+    if(window.innerWidth <= 767){
+        const profile = document.querySelector(".profile");
+        const profile_share = document.querySelector(".profile-share");
+        if(profile_share.style.display == "none"){
+            profile.style.display = "none";
+            profile_share.style.display = "flex";
+        }else{
+            profile.style.display = "flex";
+            profile_share.style.display = "none";
+        }
+        
+    }else{
+        const profile = document.querySelector(".profile");
+        const profile_share = document.querySelector(".profile-share");
+        if(profile_share.style.display == "none"){
+            profile_share.style.display = "flex";
+        }else{
+            profile_share.style.display = "none";
+        }
+    }
 }
